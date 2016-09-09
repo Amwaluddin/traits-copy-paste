375ce12d19c17c097696f5686414e555f94f0768
Rahmat Awaludin <rahmat.awaludin@gmail.com>
2016-09-09 10:04:17 +0700

    fix typo

--- Message.php
+++ Message.php
@@ -1,5 +1,5 @@
 <?php
-include "SocialThing.php"
+include "SocialThing.php";
 class Message extends SocialThing { }
 
 $message = new Message("De, mas sudah siap untuk nikah. Besok mau ke rumah ketemu Bapak. Do'akan ya.. ");
